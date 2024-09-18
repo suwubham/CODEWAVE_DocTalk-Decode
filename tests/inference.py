ab7e7cb71bbb86e2592a68dcb522ca6100250df0
Shubham Shakya <shubhamshakya369@gmail.com>
2024-09-18 14:08:17 +0545

    add llama3.1 inference

---
+++ tests/inference.py
@@ -0,0 +1,40 @@
+import os
+from dotenv import load_dotenv
+import openai
+
+load_dotenv()
+
+client = openai.OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY"),
+    base_url="https://api.sambanova.ai/v1",
+)
+
+conversation_history = [
+    {"role": "system", "content": "You are a doctor. Provide user with helpful suggestions."}
+]
+
+
+def chat_with_bot(user_input):
+    conversation_history.append({"role": "user", "content": user_input})
+
+    response = client.chat.completions.create(
+        model='Meta-Llama-3.1-8B-Instruct',
+        messages=conversation_history,
+        temperature=0.1,
+        top_p=0.1
+    )
+
+    assistant_reply = response.choices[0].message.content
+    conversation_history.append(
+        {"role": "assistant", "content": assistant_reply})
+
+    return assistant_reply
+
+
+while True:
+    user_input = input("You: ")
+    if user_input.lower() == "exit":
+        break
+
+    assistant_reply = chat_with_bot(user_input)
+    print("Assistant:", assistant_reply)
