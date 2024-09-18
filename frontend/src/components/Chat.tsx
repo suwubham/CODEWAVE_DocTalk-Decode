6b8265fa1d8d802539097b9651aafe5a3ac67a6b
Saral Sainju <sainju.saral433@gmail.com>
2024-09-19 01:52:03 +0545

    chatbot

--- frontend/src/components/Chat.tsx
+++ frontend/src/components/Chat.tsx
@@ -16,14 +16,14 @@ export const Chat = () => {
     text: msg.content[0].text,
   }));
   return (
-    <>
+    <div className="h-screen text-white">
       <ChatMessageList>
         {filteredMessages.map((msg: any, index: any) => (
           <ChatBubble
             key={index}
             variant={msg.role === "user" ? "sent" : "received"}
           >
-            <ChatBubbleAvatar fallback={msg.role === "user" ? "US" : "AI"} />
+            <ChatBubbleAvatar fallback={msg.role === "user" ? "US" : "AI"} className="bg-white text-black"/>
             <ChatBubbleMessage
               variant={msg.role === "user" ? "sent" : "received"}
             >
@@ -35,15 +35,15 @@ export const Chat = () => {
       <form className="relative rounded-lg border bg-background focus-within:ring-1 focus-within:ring-ring p-1 w-full">
         <ChatInput
           placeholder="Type your message here..."
-          className="min-h-12 resize-none rounded-lg bg-background border-0 p-3 shadow-none focus-visible:ring-0 w-full"
+          className="text-black min-h-12 resize-none rounded-lg bg-background border-0 p-3 shadow-none focus-visible:ring-0 w-full"
         />
-        <div className="flex items-center p-3 pt-0">
+        <div className="flex items-center p-3 pt-0 text-black">
           <Button size="sm" className="ml-auto gap-1.5">
             Send Message
             <CornerDownLeft className="size-3.5" />
           </Button>
         </div>
       </form>
-    </>
+    </div>
   );
 };
